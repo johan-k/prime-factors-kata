2167ef34dc89452be53ba9322f07f2941289dbb8
Johan Kerdekachian <johan.kerdekachian@gmail.com>
2024-04-12 20:48:26 +0200

    init project

---
+++ src/main/java/com/example/primefactorskata/PrimeFactorsKataApplication.java
@@ -0,0 +1,13 @@
+package com.example.primefactorskata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeFactorsKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeFactorsKataApplication.class, args);
+	}
+
+}
