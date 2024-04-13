bad96de3ccf102179b8d41c0d0c47d030d4b7ec7
Johan Kerdekachian <johan.kerdekachian@gmail.com>
2024-04-13 17:09:42 +0200

    test 1 ok, rédaction test 2

--- src/main/java/com/example/primefactorskata/model/PrimeFactors.java
+++ src/main/java/com/example/primefactorskata/model/PrimeFactors.java
@@ -1,10 +1,11 @@
 package com.example.primefactorskata.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactors {
 
     public static List<Integer> generate(int number) {
-        return null;
+        return new ArrayList<>();
     }
 }
--- src/test/java/com/example/primefactorskata/model/PrimeFactorsTest.java
+++ src/test/java/com/example/primefactorskata/model/PrimeFactorsTest.java
@@ -11,4 +11,8 @@ public class PrimeFactorsTest {
     public void testOne() {
         assertEquals(list(), PrimeFactors.generate(1));
     }
+    @Test
+    public void testTwo() {
+        assertEquals(list(2), PrimeFactors.generate(2));
+    }
 }
