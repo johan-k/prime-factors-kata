a00636dd15114cedd2c782cb571891ced022276e
Johan Kerdekachian <johan.kerdekachian@gmail.com>
2024-04-13 17:23:08 +0200

    test 4 ok, write test 6

--- src/main/java/com/example/primefactorskata/model/PrimeFactors.java
+++ src/main/java/com/example/primefactorskata/model/PrimeFactors.java
@@ -6,6 +6,9 @@ import java.util.List;
 public class PrimeFactors {
 
     public static List<Integer> generate(int number) {
+        if (number == 4) {
+            return List.of(2, 2);
+        }
         if (number != 1) {
             return List.of(number);
         }
--- src/test/java/com/example/primefactorskata/model/PrimeFactorsTest.java
+++ src/test/java/com/example/primefactorskata/model/PrimeFactorsTest.java
@@ -25,4 +25,9 @@ public class PrimeFactorsTest {
     public void testFour() {
         assertEquals(list(2,2), PrimeFactors.generate(4));
     }
+
+    @Test
+    public void testSix() {
+        assertEquals(list(2,3), PrimeFactors.generate(6));
+    }
 }
